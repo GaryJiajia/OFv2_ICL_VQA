7fe4ac200e07732649c2b14cec23d57fe5fab1b2
GaryJiajia <pengjiawei@seu.edu.cn>
2024-08-27 09:59:21 +0800

    parser file

---
+++ open_flamingo/eval/eval_parser.py
@@ -0,0 +1,216 @@
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--model",
+    type=str,
+    help="Model name. Currently only `OpenFlamingo` is supported.",
+    default="open_flamingo",
+)
+parser.add_argument(
+    "--results_file", type=str, default=None, help="JSON file to save results"
+)
+# Trial arguments
+parser.add_argument("--shots", nargs="+", default=[0, 4, 8, 16, 32], type=int)
+parser.add_argument(
+    "--num_trials",
+    type=int,
+    default=1,
+    help="Number of trials to run for each shot using different demonstrations",
+)
+parser.add_argument(
+    "--trial_seeds",
+    nargs="+",
+    type=int,
+    default=[42],
+    help="Seeds to use for each trial for picking demonstrations and eval sets",
+)
+parser.add_argument(
+    "--num_samples",
+    type=int,
+    default=-1,
+    help="Number of samples to evaluate on. -1 for all samples.",
+)
+parser.add_argument(
+    "--query_set_size", type=int, default=2048, help="Size of demonstration query set"
+)
+
+parser.add_argument("--batch_size", type=int, default=8)
+
+parser.add_argument(
+    "--no_caching_for_classification",
+    action="store_true",
+    help="Use key-value caching for classification evals to speed it up. Currently this doesn't underperforms for MPT models.",
+)
+
+# Per-dataset evaluation flags
+
+parser.add_argument(
+    "--eval_vizwiz",
+    action="store_true",
+    default=False,
+    help="Whether to evaluate on VizWiz.",
+)
+
+parser.add_argument(
+    "--eval_ok_vqa",
+    action="store_true",
+    default=False,
+    help="Whether to evaluate on OK-VQA.",
+)
+
+parser.add_argument(
+    "--eval_vqav2",
+    action="store_true",
+    default=False,
+    help="Whether to evaluate on VQAV2.",
+)
+
+# Dataset arguments
+## OK-VQA Dataset
+parser.add_argument(
+    "--ok_vqa_train_image_dir_path",
+    type=str,
+    help="Path to the vqav2/train2014 directory.",
+    default=None,
+)
+parser.add_argument(
+    "--ok_vqa_train_questions_json_path",
+    type=str,
+    help="Path to the v2_OpenEnded_mscoco_train2014_questions.json file.",
+    default=None,
+)
+parser.add_argument(
+    "--ok_vqa_train_annotations_json_path",
+    type=str,
+    help="Path to the v2_mscoco_train2014_annotations.json file.",
+    default=None,
+)
+parser.add_argument(
+    "--ok_vqa_test_image_dir_path",
+    type=str,
+    help="Path to the vqav2/val2014 directory.",
+    default=None,
+)
+parser.add_argument(
+    "--ok_vqa_test_questions_json_path",
+    type=str,
+    help="Path to the v2_OpenEnded_mscoco_val2014_questions.json file.",
+    default=None,
+)
+parser.add_argument(
+    "--ok_vqa_test_annotations_json_path",
+    type=str,
+    help="Path to the v2_mscoco_val2014_annotations.json file.",
+    default=None,
+)
+
+## VizWiz Dataset
+parser.add_argument(
+    "--vizwiz_train_image_dir_path",
+    type=str,
+    help="Path to the vizwiz train images directory.",
+    default=None,
+)
+parser.add_argument(
+    "--vizwiz_test_image_dir_path",
+    type=str,
+    help="Path to the vizwiz test images directory.",
+    default=None,
+)
+parser.add_argument(
+    "--vizwiz_train_questions_json_path",
+    type=str,
+    help="Path to the vizwiz questions json file.",
+    default=None,
+)
+parser.add_argument(
+    "--vizwiz_train_annotations_json_path",
+    type=str,
+    help="Path to the vizwiz annotations json file.",
+    default=None,
+)
+parser.add_argument(
+    "--vizwiz_test_questions_json_path",
+    type=str,
+    help="Path to the vizwiz questions json file.",
+    default=None,
+)
+parser.add_argument(
+    "--vizwiz_test_annotations_json_path",
+    type=str,
+    help="Path to the vizwiz annotations json file.",
+    default=None,
+)
+## VQAV2 Dataset
+parser.add_argument(
+    "--vqav2_train_image_dir_path",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--vqav2_train_questions_json_path",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--vqav2_train_annotations_json_path",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--vqav2_test_image_dir_path",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--vqav2_test_questions_json_path",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--vqav2_test_annotations_json_path",
+    type=str,
+    default=None,
+)
+
+# Distributed evaluation
+parser.add_argument(
+    "--dist-url",
+    default="env://",
+    type=str,
+    help="url used to set up distributed training",
+)
+parser.add_argument(
+    "--dist-backend", default="nccl", type=str, help="distributed backend"
+)
+parser.add_argument(
+    "--horovod",
+    default=False,
+    action="store_true",
+    help="Use horovod for distributed training.",
+)
+parser.add_argument(
+    "--no-set-device-rank",
+    default=False,
+    action="store_true",
+    help="Don't set device index from local rank (when CUDA_VISIBLE_DEVICES restricted to one per proc).",
+)
+parser.add_argument(
+    "--device",
+    type=int,
+    default=0,
+    help="device of GPUs.",
+)
+parser.add_argument(
+    "--retrieval_name",
+    type=str,
+    default="name",
+    help="name of json result file",
+)
+parser.add_argument(
+    "--dataset_name",
+    type=str,
+    default="vqav2",
+    help="name of dataset name"
+)
